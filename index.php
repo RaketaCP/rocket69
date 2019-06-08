dee16ee91ec604c36c2cb4c45941c0e51388704e
PublicStaticJohnGalt <m.abdulmedzhidov@gmail.com>
2019-06-08 15:48:15 +0300

    Подключение ACF

--- index.php
+++ index.php
@@ -4,12 +4,6 @@
  * @subpackage rocket
  * @since 0.1
  */
-wp_deregister_script('jquery' );
-wp_register_script('jquery', 'https://code.jquery.com/jquery-3.4.1.min.js');
-wp_enqueue_script('jquery' );
-wp_enqueue_script('chartjs','https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.js');
-wp_enqueue_style('chartcss','https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.css');
-wp_enqueue_script('main', get_template_directory_uri().'/script.js', array('jquery'));
 get_header();
 ?>
 <canvas id="myChart" width="400" height="400"></canvas>
