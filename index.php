d982dcd13ab3a529fa0a789c0a8ef5c676f60f23
ckolobov <kolobovconstantine@gmail.com>
2019-06-08 15:40:05 +0300

    datatables js

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ wp_deregister_script('jquery' );
 wp_register_script('jquery', 'https://code.jquery.com/jquery-3.4.1.min.js');
 wp_enqueue_script('jquery' );
 wp_enqueue_script('chartjs','https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.js');
+wp_enqueue_script('chartjs','https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js');
 wp_enqueue_style('chartcss','https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.css');
 wp_enqueue_script('main', get_template_directory_uri().'/script.js', array('jquery'));
 get_header();
